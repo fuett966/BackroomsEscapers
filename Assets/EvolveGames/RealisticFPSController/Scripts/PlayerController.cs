1f9e54f7f6140880b8dd64d195d836c21fdd5d6e
kitsunnet | Мацко Егор <76002087+fuett966@users.noreply.github.com>
2023-11-02 11:42:36 +0300

    networking on player

--- Assets/EvolveGames/RealisticFPSController/Scripts/PlayerController.cs
+++ Assets/EvolveGames/RealisticFPSController/Scripts/PlayerController.cs
@@ -2,70 +2,145 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using FishNet.Connection;
+using FishNet.Object;
 
 namespace EvolveGames
 {
     [RequireComponent(typeof(CharacterController))]
-    public class PlayerController : MonoBehaviour
+    public class PlayerController : NetworkBehaviour
     {
         [Header("PlayerController")]
-        [SerializeField] public Transform Camera;
-        [SerializeField] public ItemChange Items;
-        [SerializeField, Range(1, 10)] float walkingSpeed = 3.0f;
-        [Range(0.1f, 5)] public float CroughSpeed = 1.0f;
-        [SerializeField, Range(2, 20)] float RuningSpeed = 4.0f;
-        [SerializeField, Range(0, 20)] float jumpSpeed = 6.0f;
-        [SerializeField, Range(0.5f, 10)] float lookSpeed = 2.0f;
-        [SerializeField, Range(10, 120)] float lookXLimit = 80.0f;
+        [SerializeField]
+        public Transform Camera;
+
+        [SerializeField]
+        public ItemChange Items;
+
+        [SerializeField, Range(1, 10)]
+        float walkingSpeed = 3.0f;
+
+        [Range(0.1f, 5)]
+        public float CroughSpeed = 1.0f;
+
+        [SerializeField, Range(2, 20)]
+        float RuningSpeed = 4.0f;
+
+        [SerializeField, Range(0, 20)]
+        float jumpSpeed = 6.0f;
+
+        [SerializeField, Range(0.5f, 10)]
+        float lookSpeed = 2.0f;
+
+        [SerializeField, Range(10, 120)]
+        float lookXLimit = 80.0f;
+
         [Space(20)]
         [Header("Advance")]
-        [SerializeField] float RunningFOV = 65.0f;
-        [SerializeField] float SpeedToFOV = 4.0f;
-        [SerializeField] float CroughHeight = 1.0f;
-        [SerializeField] float gravity = 20.0f;
-        [SerializeField] float timeToRunning = 2.0f;
-        [HideInInspector] public bool canMove = true;
-        [HideInInspector] public bool CanRunning = true;
+        [SerializeField]
+        float RunningFOV = 65.0f;
+
+        [SerializeField]
+        float SpeedToFOV = 4.0f;
+
+        [SerializeField]
+        float CroughHeight = 1.0f;
+
+        [SerializeField]
+        float gravity = 20.0f;
+
+        [SerializeField]
+        float timeToRunning = 2.0f;
+
+        [HideInInspector]
+        public bool canMove = true;
+
+        [HideInInspector]
+        public bool CanRunning = true;
 
         [Space(20)]
         [Header("Climbing")]
-        [SerializeField] bool CanClimbing = true;
-        [SerializeField, Range(1, 25)] float Speed = 2f;
+        [SerializeField]
+        bool CanClimbing = true;
+
+        [SerializeField, Range(1, 25)]
+        float Speed = 2f;
         bool isClimbing = false;
 
         [Space(20)]
         [Header("HandsHide")]
-        [SerializeField] bool CanHideDistanceWall = true;
-        [SerializeField, Range(0.1f, 5)] float HideDistance = 1.5f;
-        [SerializeField] int LayerMaskInt = 1;
+        [SerializeField]
+        bool CanHideDistanceWall = true;
+
+        [SerializeField, Range(0.1f, 5)]
+        float HideDistance = 1.5f;
+
+        [SerializeField]
+        int LayerMaskInt = 1;
 
         [Space(20)]
         [Header("Input")]
-        [SerializeField] KeyCode CroughKey = KeyCode.LeftControl;
+        [SerializeField]
+        KeyCode CroughKey = KeyCode.LeftControl;
 
+        [HideInInspector]
+        public CharacterController characterController;
 
-        [HideInInspector] public CharacterController characterController;
-        [HideInInspector] public Vector3 moveDirection = Vector3.zero;
+        [HideInInspector]
+        public Vector3 moveDirection = Vector3.zero;
         bool isCrough = false;
         float InstallCroughHeight;
         float rotationX = 0;
-        [HideInInspector] public bool isRunning = false;
+
+        [HideInInspector]
+        public bool isRunning = false;
         Vector3 InstallCameraMovement;
         float InstallFOV;
         Camera cam;
-        [HideInInspector] public bool Moving;
-        [HideInInspector] public float vertical;
-        [HideInInspector] public float horizontal;
-        [HideInInspector] public float Lookvertical;
-        [HideInInspector] public float Lookhorizontal;
+
+        [HideInInspector]
+        public bool Moving;
+
+        [HideInInspector]
+        public float vertical;
+
+        [HideInInspector]
+        public float horizontal;
+
+        [HideInInspector]
+        public float Lookvertical;
+
+        [HideInInspector]
+        public float Lookhorizontal;
         float RunningValue;
         float installGravity;
         bool WallDistance;
-        [HideInInspector] public float WalkingValue;
-        void Start()
+
+        [HideInInspector]
+        public float WalkingValue;
+
+        // void Start()
+        // {
+        //     characterController = GetComponent<CharacterController>();
+        //     if (Items == null && GetComponent<ItemChange>())
+        //         Items = GetComponent<ItemChange>();
+        //     cam = GetComponentInChildren<Camera>();
+        //     Cursor.lockState = CursorLockMode.Locked;
+        //     Cursor.visible = false;
+        //     InstallCroughHeight = characterController.height;
+        //     InstallCameraMovement = Camera.localPosition;
+        //     InstallFOV = cam.fieldOfView;
+        //     RunningValue = RuningSpeed;
+        //     installGravity = gravity;
+        //     WalkingValue = walkingSpeed;
+        // }
+
+        public override void OnStartClient()
         {
             characterController = GetComponent<CharacterController>();
-            if (Items == null && GetComponent<ItemChange>()) Items = GetComponent<ItemChange>();
+            if (Items == null && GetComponent<ItemChange>())
+                Items = GetComponent<ItemChange>();
             cam = GetComponentInChildren<Camera>();
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
@@ -79,6 +154,9 @@ namespace EvolveGames
 
         void Update()
         {
+            if(!base.IsOwner)
+            { return; 
+            }
             RaycastHit CroughCheck;
             RaycastHit ObjectCheck;
 
@@ -88,11 +166,25 @@ namespace EvolveGames
             }
             Vector3 forward = transform.TransformDirection(Vector3.forward);
             Vector3 right = transform.TransformDirection(Vector3.right);
-            isRunning = !isCrough ? CanRunning ? Input.GetKey(KeyCode.LeftShift) : false : false;
-            vertical = canMove ? (isRunning ? RunningValue : WalkingValue) * Input.GetAxis("Vertical") : 0;
-            horizontal = canMove ? (isRunning ? RunningValue : WalkingValue) * Input.GetAxis("Horizontal") : 0;
-            if (isRunning) RunningValue = Mathf.Lerp(RunningValue, RuningSpeed, timeToRunning * Time.deltaTime);
-            else RunningValue = WalkingValue;
+            isRunning = !isCrough
+                ? CanRunning
+                    ? Input.GetKey(KeyCode.LeftShift)
+                    : false
+                : false;
+            vertical = canMove
+                ? (isRunning ? RunningValue : WalkingValue) * Input.GetAxis("Vertical")
+                : 0;
+            horizontal = canMove
+                ? (isRunning ? RunningValue : WalkingValue) * Input.GetAxis("Horizontal")
+                : 0;
+            if (isRunning)
+                RunningValue = Mathf.Lerp(
+                    RunningValue,
+                    RuningSpeed,
+                    timeToRunning * Time.deltaTime
+                );
+            else
+                RunningValue = WalkingValue;
             float movementDirectionY = moveDirection.y;
             moveDirection = (forward * vertical) + (right * horizontal);
 
@@ -105,7 +197,8 @@ namespace EvolveGames
                 moveDirection.y = movementDirectionY;
             }
             characterController.Move(moveDirection * Time.deltaTime);
-            Moving = horizontal < 0 || vertical < 0 || horizontal > 0 || vertical > 0 ? true : false;
+            Moving =
+                horizontal < 0 || vertical < 0 || horizontal > 0 || vertical > 0 ? true : false;
 
             if (Cursor.lockState == CursorLockMode.Locked && canMove)
             {
@@ -117,32 +210,73 @@ namespace EvolveGames
                 Camera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
                 transform.rotation *= Quaternion.Euler(0, Lookhorizontal * lookSpeed, 0);
 
-                if (isRunning && Moving) cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, RunningFOV, SpeedToFOV * Time.deltaTime);
-                else cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, InstallFOV, SpeedToFOV * Time.deltaTime);
+                if (isRunning && Moving)
+                    cam.fieldOfView = Mathf.Lerp(
+                        cam.fieldOfView,
+                        RunningFOV,
+                        SpeedToFOV * Time.deltaTime
+                    );
+                else
+                    cam.fieldOfView = Mathf.Lerp(
+                        cam.fieldOfView,
+                        InstallFOV,
+                        SpeedToFOV * Time.deltaTime
+                    );
             }
 
             if (Input.GetKey(CroughKey))
             {
                 isCrough = true;
-                float Height = Mathf.Lerp(characterController.height, CroughHeight, 5 * Time.deltaTime);
+                float Height = Mathf.Lerp(
+                    characterController.height,
+                    CroughHeight,
+                    5 * Time.deltaTime
+                );
                 characterController.height = Height;
                 WalkingValue = Mathf.Lerp(WalkingValue, CroughSpeed, 6 * Time.deltaTime);
-
             }
-            else if (!Physics.Raycast(GetComponentInChildren<Camera>().transform.position, transform.TransformDirection(Vector3.up), out CroughCheck, 0.8f, 1))
+            else if (
+                !Physics.Raycast(
+                    GetComponentInChildren<Camera>().transform.position,
+                    transform.TransformDirection(Vector3.up),
+                    out CroughCheck,
+                    0.8f,
+                    1
+                )
+            )
             {
                 if (characterController.height != InstallCroughHeight)
                 {
                     isCrough = false;
-                    float Height = Mathf.Lerp(characterController.height, InstallCroughHeight, 6 * Time.deltaTime);
+                    float Height = Mathf.Lerp(
+                        characterController.height,
+                        InstallCroughHeight,
+                        6 * Time.deltaTime
+                    );
                     characterController.height = Height;
                     WalkingValue = Mathf.Lerp(WalkingValue, walkingSpeed, 4 * Time.deltaTime);
                 }
             }
 
-            if(WallDistance != Physics.Raycast(GetComponentInChildren<Camera>().transform.position, transform.TransformDirection(Vector3.forward), out ObjectCheck, HideDistance, LayerMaskInt) && CanHideDistanceWall)
+            if (
+                WallDistance
+                    != Physics.Raycast(
+                        GetComponentInChildren<Camera>().transform.position,
+                        transform.TransformDirection(Vector3.forward),
+                        out ObjectCheck,
+                        HideDistance,
+                        LayerMaskInt
+                    )
+                && CanHideDistanceWall
+            )
             {
-                WallDistance = Physics.Raycast(GetComponentInChildren<Camera>().transform.position, transform.TransformDirection(Vector3.forward), out ObjectCheck, HideDistance, LayerMaskInt);
+                WallDistance = Physics.Raycast(
+                    GetComponentInChildren<Camera>().transform.position,
+                    transform.TransformDirection(Vector3.forward),
+                    out ObjectCheck,
+                    HideDistance,
+                    LayerMaskInt
+                );
                 Items.ani.SetBool("Hide", WallDistance);
                 Items.DefiniteHide = WallDistance;
             }
@@ -151,20 +285,26 @@ namespace EvolveGames
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Ladder" && CanClimbing)
-            { 
+            {
                 CanRunning = false;
                 isClimbing = true;
                 WalkingValue /= 2;
                 Items.Hide(true);
             }
         }
+
         private void OnTriggerStay(Collider other)
         {
             if (other.tag == "Ladder" && CanClimbing)
             {
-                moveDirection = new Vector3(0, Input.GetAxis("Vertical") * Speed * (-Camera.localRotation.x / 1.7f), 0);
+                moveDirection = new Vector3(
+                    0,
+                    Input.GetAxis("Vertical") * Speed * (-Camera.localRotation.x / 1.7f),
+                    0
+                );
             }
         }
+
         private void OnTriggerExit(Collider other)
         {
             if (other.tag == "Ladder" && CanClimbing)
@@ -176,6 +316,5 @@ namespace EvolveGames
                 Items.Hide(false);
             }
         }
-
     }
 }
