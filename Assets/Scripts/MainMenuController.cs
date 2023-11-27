9c455b053e8369cec69858ce0871483ebb5a88ed
kitsunnet | Мацко Егор <76002087+fuett966@users.noreply.github.com>
2023-11-28 02:01:55 +0300

    upd

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject startMenuPanel;
+
+    [SerializeField]
+    private GameObject multiplayerMenuPanel;
+
+    [SerializeField]
+    private GameObject settingsMenuPanel;
+
+    public void SwitchStartMenuPanel()
+    {
+        startMenuPanel.SetActive(!startMenuPanel.activeSelf);
+        //Close other menus
+    }
+
+    public void SwitchMultiplayerMenuPanel()
+    {
+        multiplayerMenuPanel.SetActive(!multiplayerMenuPanel.activeSelf);
+        //Close other menus
+    }
+
+    public void SwitchSettingsMenuPanel()
+    {
+        settingsMenuPanel.SetActive(!settingsMenuPanel.activeSelf);
+        //Close other menus
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
