87c88af8009ed34912e38c8b6f8a02ed468bad9e
Yokomi <38473135+Yokomide@users.noreply.github.com>
2023-11-28 23:57:36 +0300

    Update
    chg: door script

--- Assets/DoorInteractor.cs
+++ Assets/DoorInteractor.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Mirror;
-using UnityEngine.Rendering;
+using UnityEngine;
 
 public class DoorInteractor : NetworkBehaviour
 {
@@ -11,31 +8,82 @@ public class DoorInteractor : NetworkBehaviour
     GameObject dragPointGameobject;
     int leftDoor = 0;
     [SerializeField] LayerMask doorLayer;
+    private bool isHolding;
 
-    [Command]
-    private void CmdDoorInteract()
+    void Update()
     {
-        DoorInteractLogic();
+        if (isOwned)
+        {
+            Debug.Log(isOwned);
+            LocalInteract();
+        }
+    }
+    [Command]
+    private void CmdInteractHold(float delta)
+    {
+        ServerInteractHold(delta);
     }
 
-   private void DoorInteractLogic()
+    private void ServerInteractHold(float delta)
     {
+        if (selectedDoor != null)
+        {
+            HingeJoint joint = selectedDoor.GetComponent<HingeJoint>();
+            JointMotor motor = joint.motor;
+            float speedMultiplier = 60000;
+
+            if (isHolding)
+            {
+                if (Mathf.Abs(selectedDoor.parent.forward.z) > 0.5f)
+                {
+                    if (dragPointGameobject.transform.position.x > selectedDoor.position.x)
+                    {
+                        motor.targetVelocity = delta * -speedMultiplier * Time.deltaTime * leftDoor;
+                    }
+                    else
+                    {
+                        motor.targetVelocity = delta * speedMultiplier * Time.deltaTime * leftDoor;
+                    }
+                }
+                else
+                {
+                    if (dragPointGameobject.transform.position.z > selectedDoor.position.z)
+                    {
+                        motor.targetVelocity = delta * -speedMultiplier * Time.deltaTime * leftDoor;
+                    }
+                    else
+                    {
+                        motor.targetVelocity = delta * speedMultiplier * Time.deltaTime * leftDoor;
+                    }
+                }
+                joint.motor = motor;
+            }
+            else
+            {
+                motor.targetVelocity = 0;
+                joint.motor = motor;
+                selectedDoor = null;
+            }
+        }
+    }
+
+
+    private void LocalInteract()
+    {
+        Debug.Log("LocalInteract");
         RaycastHit hit;
 
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 20, doorLayer))
         {
             if (Input.GetMouseButtonDown(0))
             {
+                Debug.Log("MouseDown");
+                isHolding = true;
                 selectedDoor = hit.collider.gameObject.transform;
             }
         }
-
         if (selectedDoor != null)
         {
-            HingeJoint joint = selectedDoor.GetComponent<HingeJoint>();
-            JointMotor motor = joint.motor;
-
-            //Create drag point object for reference where players mouse is pointing
             if (dragPointGameobject == null)
             {
                 dragPointGameobject = new GameObject("Ray door");
@@ -49,7 +97,6 @@ public class DoorInteractor : NetworkBehaviour
 
             float delta = Mathf.Pow(Vector3.Distance(dragPointGameobject.transform.position, selectedDoor.position), 3);
 
-            //Deciding if it is left or right door
             if (selectedDoor.GetComponent<MeshRenderer>().localBounds.center.x > selectedDoor.localPosition.x)
             {
                 leftDoor = 1;
@@ -58,57 +105,24 @@ public class DoorInteractor : NetworkBehaviour
             {
                 leftDoor = -1;
             }
-
-            //Applying velocity to door motor
-            float speedMultiplier = 60000;
-            if (Mathf.Abs(selectedDoor.parent.forward.z) > 0.5f)
+            if (Input.GetMouseButtonUp(0))
             {
-                if (dragPointGameobject.transform.position.x > selectedDoor.position.x)
-                {
-                    motor.targetVelocity = delta * -speedMultiplier * Time.deltaTime * leftDoor;
-                }
-                else
-                {
-                    motor.targetVelocity = delta * speedMultiplier * Time.deltaTime * leftDoor;
-                }
+                isHolding = false;
+                Destroy(dragPointGameobject);
+            }
+            if (!isServer)
+            {
+                CmdInteractHold(delta);
             }
             else
             {
-                if (dragPointGameobject.transform.position.z > selectedDoor.position.z)
-                {
-                    motor.targetVelocity = delta * -speedMultiplier * Time.deltaTime * leftDoor;
-                }
-                else
-                {
-                    motor.targetVelocity = delta * speedMultiplier * Time.deltaTime * leftDoor;
-                }
-            }
-            joint.motor = motor;
-
-            if (Input.GetMouseButtonUp(0))
-            {
-                selectedDoor = null;
-                motor.targetVelocity = 0;
-                joint.motor = motor;
-                Destroy(dragPointGameobject);
+                ServerInteractHold(delta);
             }
         }
-    }
-
-
-
-    void Update()
-    {
-        if (!isServer)
-        {
-            CmdDoorInteract();
-        }
-        else
-        {
-            DoorInteractLogic();
-        }
-       
     }
 }
+
+
+
 
 
